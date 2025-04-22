299befad7cc8eebb985bc23bed1204ec3ea07667
Gloria Ciavarrini <gciavarrini@redhat.com>
2025-04-22 12:10:45 +0200

    Add versioning
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

---
+++ version.go
@@ -0,0 +1,14 @@
+// Assisted by watsonx Code Assistant
+
+//go:build ignore
+// +build ignore
+
+package main
+
+import "fmt"
+
+var Version = "1.0.5"
+
+func Main() {
+	fmt.Println("Version:", Version)
+}
